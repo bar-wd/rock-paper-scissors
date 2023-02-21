04c3366e546f58288949e30490d4d6fa6f4575d9
Ryan Korinek <korinekonie@gmail.com>
2023-02-21 10:14:10 -0800

    Correctly display ties using rock
    Fixed the bug when player and computer tie with rock, the computer
    would get a score.

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-"use strict";
+'use strict';
 
 function getComputerChoice() {
   let random = Math.floor(Math.random() * 3 + 1);
   let result;
 
   if (random === 1) {
-    result = "rock";
+    result = 'rock';
   }
 
   if (random === 2) {
-    result = "paper";
+    result = 'paper';
   }
 
   if (random === 3) {
-    result = "scissors";
+    result = 'scissors';
   }
   return result;
 }
@@ -22,40 +22,76 @@ function playRound(playerSelection, computerSelection) {
   const player = playerSelection.toLowerCase();
   let result;
 
-  if (player === "rock" && computerSelection === "scissors") {
-    result = "You win! Rock beats scissors!";
+  if (player === 'rock' && computerSelection === 'scissors') {
+    result = `
+    You Chose ${playerSelection}
+    Computer Chose ${computerSelection}
+
+    You win! Rock beats scissors!`;
   }
 
-  if (player === "rock" && computerSelection === "paper") {
-    result = "You lose! Paper beats rock!";
+  if (player === 'rock' && computerSelection === 'paper') {
+    result = `
+    You Chose ${playerSelection}
+    Computer Chose ${computerSelection}
+
+    You lose! Paper beats rock!`;
   }
 
-  if (player === "paper" && computerSelection === "scissors") {
-    result = "You lose! Paper beats scissors!";
+  if (player === 'paper' && computerSelection === 'scissors') {
+    result = `
+    You Chose ${playerSelection}
+    Computer Chose ${computerSelection}
+    
+    You lose! Paper beats scissors!`;
   }
 
-  if (player === "paper" && computerSelection === "rock") {
-    result = "You win! Paper beats rock!";
+  if (player === 'paper' && computerSelection === 'rock') {
+    result = `
+    You Chose ${playerSelection}
+    Computer Chose ${computerSelection}
+    
+    You win! Paper beats rock!`;
   }
 
-  if (player === "scissors" && computerSelection === "rock") {
-    result = "You lose! Rock beats scissors!";
+  if (player === 'scissors' && computerSelection === 'rock') {
+    result = `
+    You Chose ${playerSelection}
+    Computer Chose ${computerSelection}
+    
+    You lose! Rock beats scissors!`;
   }
 
-  if (player === "scissors" && computerSelection === "paper") {
-    result = "You win! Scissors beats paper!";
+  if (player === 'scissors' && computerSelection === 'paper') {
+    result = `
+    You Chose ${playerSelection}
+    Computer Chose ${computerSelection}
+    
+    You win! Scissors beats paper!`;
   }
 
-  if (player === "scissors" && computerSelection === "scissors") {
-    result = "Scissors ties scissors.";
+  if (player === 'scissors' && computerSelection === 'scissors') {
+    result = `
+    You Chose ${playerSelection}
+    Computer Chose ${computerSelection}
+    
+    Scissors ties scissors.`;
   }
 
-  if (player === "rock" && computerSelection === "rock") {
-    result = "Rock ties rock.";
+  if (player === 'rock' && computerSelection === 'rock') {
+    result = `
+    You Chose ${playerSelection}
+    Computer Chose ${computerSelection}
+    
+    Rock ties rock.`;
   }
 
-  if (player === "paper" && computerSelection === "paper") {
-    result = "Paper ties paper.";
+  if (player === 'paper' && computerSelection === 'paper') {
+    result = `
+    You Chose ${playerSelection}
+    Computer Chose ${computerSelection}
+    
+    Paper ties paper.`;
   }
 
   return result;
@@ -69,16 +105,14 @@ function game() {
   while (round <= 5) {
     let result = playRound(playerSelection, getComputerChoice());
 
-    if (result.includes("ties")) {
+    if (result.includes('ties')) {
       console.log(`
  Round number ${round} of 5: ${result}
     
         Player Score = ${playerScore}
         Computer Score = ${computerScore}
             `);
-    }
-
-    if (result.includes("win")) {
+    } else if (result.includes('win')) {
       playerScore++;
 
       console.log(`Round number ${round} of 5: ${result}
@@ -99,15 +133,13 @@ function game() {
 
   console.log(
     playerScore > computerScore
-      ? "You have defeated the computer. Congratulations."
+      ? `You have defeated the computer ${playerScore} to ${computerScore}. Congratulations.`
       : computerScore === playerScore
-      ? "Tie game."
-      : "You lose. Train harder."
+      ? 'Tie game.'
+      : `You lose ${playerScore} to ${computerScore}. Train harder.`
   );
 }
-const playerSelection = prompt();
-const computerSelection = getComputerChoice(
-  "Please enter rock, paper, or scissors."
-);
+const playerSelection = prompt('Please enter rock, paper, or scissors.');
+const computerSelection = getComputerChoice();
 // console.log(playRound(playerSelection, computerSelection));
 game();
