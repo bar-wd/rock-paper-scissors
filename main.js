737f4a33e44f4475c34b6cde8a9eacb704eeff12
Ryan Korinek <korinekonie@gmail.com>
2023-02-22 14:30:35 -0800

    Refactor code and update background images
    Refactored the code and updated the background image.

--- main.js
+++ main.js
@@ -7,13 +7,6 @@ const winLoseMsg = document.querySelector('.main-one');
 const msg = document.querySelector('.main-two');
 const playerImg = document.querySelector('.player-score img');
 const computerImg = document.querySelector('.computer-score img');
-const endMsg = document.querySelector('.play-again');
-const blur = document.querySelectorAll('.end-game');
-const btn = document.querySelector('.btn');
-const endMsgTxt = document.querySelector('.play-again div');
-const rock = document.querySelector('.rock-hover');
-const paper = document.querySelector('.paper-hover');
-const scissors = document.querySelector('.scissors-hover');
 const header = document.querySelector('div.header');
 const main = document.querySelector('div.main');
 const container = document.querySelector('div.container');
@@ -23,133 +16,55 @@ const playAgain = document.querySelector('.container h2');
 
 let playerSelection;
 let computerSelection;
-let round = 1;
 let playerScore = 0;
 let computerScore = 0;
 let result;
 
-function onClick(event) {
-  playerSelection = event.target.classList[0];
-
-  game();
-  playerScoreElement.innerText = `Player Score: ${playerScore}`;
-  computerScoreElement.innerText = `Computer Score: ${computerScore}`;
-  playerImg.src = `./images/${playerSelection}-removebg-preview.png`;
-  computerImg.src = `./images/${computerSelection}-removebg-preview.png`;
-  console.log(playerImg.src);
-  if (playerScore === 5) {
-    msg.innerText = ``;
-    // endMsg.style.display = 'flex';
-    blur.forEach(ele => (ele.style.filter = 'blur(5px)'));
-    images.forEach(img => img.removeEventListener('click', onClick));
-    btn.innerText = 'Play again?';
-    endMsgTxt.innerText = 'You won!';
-    images.forEach(img => img.classList.remove('hover'));
-    header.classList.add('hidden');
-    main.classList.add('hidden');
-    container.classList.remove('hidden');
-    neon.innerText = `You won!`;
-    body.classList.add('neon-body');
-    neon.classList.add('neon-h1');
-  } else if (computerScore === 5) {
-    msg.innerText = ``;
-    blur.forEach(ele => (ele.style.filter = 'blur(5px)'));
-    // endMsg.style.display = 'flex';
-    images.forEach(img => img.removeEventListener('click', onClick));
-    btn.innerText = 'Play again?';
-    endMsgTxt.innerText = 'You lost!';
-    images.forEach(img => img.classList.remove('hover'));
-    header.classList.add('hidden');
-    main.classList.add('hidden');
-    container.classList.remove('hidden');
-    neon.innerText = `You lost!`;
-    body.classList.add('neon-body');
-    neon.classList.add('neon-h1');
-  }
-}
-
-function reset(event) {
-  playerScore = 0;
-  computerScore = 0;
-  playerScoreElement.innerText = `Player Score: ${playerScore}`;
-  computerScoreElement.innerText = `Computer Score: ${computerScore}`;
-  blur.forEach(ele => (ele.style.filter = ''));
-  endMsg.style.display = 'none';
-  msg.innerText = `First Score to 5 Wins`;
-  winLoseMsg.innerText = `Choose Carefully...`;
-  images.forEach(img => img.addEventListener('click', onClick));
-  images.forEach(img => img.classList.add('hover'));
-  body.classList.remove('neon-body');
-  container.classList.add('hidden');
-  header.classList.remove('hidden');
-  main.classList.remove('hidden');
-  playerImg.src = './images/neon-questionmark3-removebg-preview.png';
-  computerImg.src = './images/neon-questionmark3-removebg-preview.png';
-}
-
-images.forEach(img => img.addEventListener('click', onClick));
-btn.addEventListener('click', reset);
-playAgain.addEventListener('click', reset);
-
 function getComputerChoice() {
   let random = Math.floor(Math.random() * 3 + 1);
-  let result;
+  let computerChoice;
 
   if (random === 1) {
-    result = 'rock';
+    computerChoice = 'rock';
   }
 
   if (random === 2) {
-    result = 'paper';
+    computerChoice = 'paper';
   }
 
   if (random === 3) {
-    result = 'scissors';
+    computerChoice = 'scissors';
   }
-  return result;
+  return computerChoice;
 }
 
 function playRound(playerSelection, computerSelection) {
-  const player = playerSelection.toLowerCase();
-
-  if (player === 'rock' && computerSelection === 'scissors') {
+  if (playerSelection === 'rock' && computerSelection === 'scissors') {
     result = `win`;
-  }
-
-  if (player === 'rock' && computerSelection === 'paper') {
+  } else if (playerSelection === 'rock' && computerSelection === 'paper') {
     result = `lose`;
-  }
-
-  if (player === 'paper' && computerSelection === 'scissors') {
+  } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
     result = `lose`;
-  }
-
-  if (player === 'paper' && computerSelection === 'rock') {
+  } else if (playerSelection === 'paper' && computerSelection === 'rock') {
     result = `win`;
-  }
-
-  if (player === 'scissors' && computerSelection === 'rock') {
+  } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
     result = `lose`;
-  }
-
-  if (player === 'scissors' && computerSelection === 'paper') {
+  } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
     result = `win`;
-  }
-
-  if (player === 'scissors' && computerSelection === 'scissors') {
+  } else if (
+    playerSelection === 'scissors' &&
+    computerSelection === 'scissors'
+  ) {
     result = `tie`;
-  }
-
-  if (player === 'rock' && computerSelection === 'rock') {
+  } else if (playerSelection === 'rock' && computerSelection === 'rock') {
     result = `tie`;
-  }
-
-  if (player === 'paper' && computerSelection === 'paper') {
+  } else if (playerSelection === 'paper' && computerSelection === 'paper') {
     result = `tie`;
   }
 
   return result;
 }
+
 function game() {
   computerSelection = getComputerChoice();
   let result = playRound(playerSelection, computerSelection);
@@ -168,3 +83,47 @@ function game() {
   }
   return result;
 }
+
+function onClick(event) {
+  playerSelection = event.target.classList[0];
+  game();
+  playerScoreElement.innerText = `Player Score: ${playerScore}`;
+  computerScoreElement.innerText = `Computer Score: ${computerScore}`;
+  playerImg.src = `./images/${playerSelection}-removebg-preview.png`;
+  computerImg.src = `./images/${computerSelection}-removebg-preview.png`;
+
+  if (playerScore === 5 || computerScore === 5) {
+    gameOver();
+  }
+}
+
+function gameOver() {
+  images.forEach(img => img.removeEventListener('click', onClick));
+  images.forEach(img => img.classList.remove('hover'));
+  header.classList.add('hidden');
+  main.classList.add('hidden');
+  container.classList.remove('hidden');
+  neon.innerText = `You ${playerScore === 5 ? 'won' : 'lost'}!`;
+  body.classList.add('neon-body');
+  neon.classList.add('neon-h1');
+}
+
+function reset(event) {
+  playerScore = 0;
+  computerScore = 0;
+  playerScoreElement.innerText = `Player Score: ${playerScore}`;
+  computerScoreElement.innerText = `Computer Score: ${computerScore}`;
+  msg.innerText = `First Score to 5 Wins`;
+  winLoseMsg.innerText = `Choose Carefully...`;
+  images.forEach(img => img.addEventListener('click', onClick));
+  images.forEach(img => img.classList.add('hover'));
+  body.classList.remove('neon-body');
+  container.classList.add('hidden');
+  header.classList.remove('hidden');
+  main.classList.remove('hidden');
+  playerImg.src = './images/neon-questionmark3-removebg-preview.png';
+  computerImg.src = './images/neon-questionmark3-removebg-preview.png';
+}
+
+images.forEach(img => img.addEventListener('click', onClick));
+playAgain.addEventListener('click', reset);
